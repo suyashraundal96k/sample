58ad21d9b931cc03f66ac59b8e42b0320e12338c
suyashraundal96k <suyashraundal.491@gmail.com>
2025-02-03 10:23:45 +0530

    Hello

---
+++ Github commands.py
@@ -0,0 +1,46 @@
+#Set up Git (one-time):
+ #git config --global user.name "suyash" && git config --global user.email "suyashraundal.491@gmail.com"
+
+#Clone a repository:
+#git clone https://github.com/suyashraundal96k/sample.git
+
+#Initialize a new repository:
+#git init
+
+#Check status:
+#git status
+
+#Add and commit changes:
+#git add . && git commit -m "Hello"
+
+#Create and switch to a new branch:
+#git checkout -b new-branch
+
+#Switch  branch:
+#git switch branch-name
+
+#Push changes to GitHub: 
+#git push origin branch-name
+
+#Pull changes from GitHub:
+#git pull origin branch-name
+
+#Merge a branch into the current branch:
+#git merge branch-name
+
+#View commit history 
+#git log --oneline
+
+#Add a remote repository:
+#git remote add origin https://github.com/suyashraundal96k/sample.git
+
+#Undo staged changes:
+#git restore --staged file.txt
+
+#Discard local changes:
+#git restore file.txt
+
+
+
+
+
